f56526e8ee76aa1bb67346f7c312093ebf3e4aa7
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-03-24 16:22:12 +0900

    [Gold IV] Title: 네트워크 연결, Time: 400 ms, Memory: 125644 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,34 +1,29 @@
-def prim(start):
-    U = []
-    D = [INF] * (N+1)
+def find(x):
+    if p[x] != x:
+        p[x] = find(p[x])
+    return p[x]
 
-    D[start] = 0
-    while len(U) < N:
-        minV = INF
-        for i in range(1,N+1):
-            if i in U:
-                continue
+def union(x, y):
+    x = find(x)
+    y = find(y)
 
-            if D[i] < minV:
-                minV = D[i]
-                start = i
-        U.append(start)
-
-        for i in range(1,N+1):
-            if i in U:
-                continue
-
-            if graph[start][i]:
-                D[i] = min(D[i], graph[start][i])
-    print(sum(D[1:]))
+    if x < y:
+        p[y] = x
+    else:
+        p[x] = y
 
 N = int(input())
 M = int(input())
-INF = int(10e9)
-graph = [[0]*(N+1) for _ in range(N+1)]
+p = [i for i in range(N+1)]
+edges = []
 for _ in range(M):
     s, e, w = map(int, input().split())
-    graph[s][e] = w
-    graph[e][s] = w
-
-prim(1)
+    edges.append((w, s, e))
+edges.sort()
+# print(edges)
+answer = 0
+for w, s, e in edges:
+    if find(s) != find(e):
+        union(s, e)
+        answer += w
+print(answer)
