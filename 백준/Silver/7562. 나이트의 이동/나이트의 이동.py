98a1b11fe9d194b24b0afbf145ab6f06498dc4bc
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-04-09 12:15:20 +0900

    [Silver I] Title: 나이트의 이동, Time: 316 ms, Memory: 129648 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -1,28 +1,28 @@
 from collections import deque
 
-def bfs(r, c):
+def bfs(y1, x1, y2, x2):
     q = deque()
-    q.append((r,c))
-    lst[r][c] = 0
-
+    q.append((y1, x1, 0))
+    lst[y1][x1] += 1
     while q:
-        vr, vc = q.popleft()
+        y, x, cnt = q.popleft()
+        if y == y2 and x == x2:
+            return cnt
+        for dy, dx in ((1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1), (-1, -2), (-2, -1)):
+            ny = y + dy
+            nx = x + dx
+            if 0 <= ny <= I-1 and 0 <= nx <= I-1 and lst[ny][nx] == 0:
+                q.append((ny, nx, cnt+1))
+                lst[ny][nx] = cnt
+    else:
+        return 0
 
-        for dr, dc in [(2,1),(-2,1),(2,-1),(-2,-1),(1,-2),(-1,2),(1,2),(-1,-2)]:
-            newR = dr+vr
-            newC = dc+vc
-            if 0<=newR<N and 0<=newC<N and lst[newR][newC] == -1:
-                q.append((newR,newC))
-                lst[newR][newC] = lst[vr][vc]+1
+T = int(input()) # 테스트 케이스 개수
 
-
-
-T = int(input())
 for _ in range(T):
-    N = int(input())
-    lst = [[-1]*N for _ in range(N)]
-    nr, nc = map(int, input().split())
-    gr, gc = map(int, input().split())
-
-    bfs(nr, nc)
-    print(lst[gr][gc])
+    I = int(input()) # 한 변의 길이
+    lst = [[0] * I for _ in range(I)]
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+    answer = bfs(y1, x1, y2, x2)
+    print(answer)
