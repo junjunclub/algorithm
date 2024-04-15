0db0c9dc3a6f0d343a0a202f548781f19cd6226a
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-04-15 21:31:40 +0900

    [Gold III] Title: 개구리 점프, Time: 460 ms, Memory: 127860 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17619. 개구리 점프/개구리 점프.py
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def find(x):
     if p[x] != x:
         p[x] = find(p[x])
@@ -19,9 +22,17 @@ for i in range(N):
     x1, x2, y = map(int, input().split())
     lst.append((x1,x2,y,i))
 lst.sort()
+
+start, end, value, idx = lst[0]
 for v in range(1,N):
-    if lst[v-1][1] >= lst[v][0]:
-        union(lst[v-1][3],lst[v][3])
+    n_start, n_end, n_value, n_idx = lst[v]
+    # 겹치면
+    if start<=n_start<=end:
+        union(idx, n_idx)
+        if n_end >= end:
+            start, end, value, idx = n_start, n_end, n_value, n_idx
+    else:
+        start, end, value, idx = n_start, n_end, n_value, n_idx
 
 
 for _ in range(Q):
