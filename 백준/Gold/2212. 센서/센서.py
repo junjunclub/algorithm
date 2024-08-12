b0a545df464704b46441e6ec639b95a7c3e7c056
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-08-12 21:28:59 +0900

    [Gold V] Title: 센서, Time: 116 ms, Memory: 110748 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.py
@@ -0,0 +1,9 @@
+N = int(input())
+K = int(input())
+sensor = list(map(int, input().split()))
+sensor.sort()
+lst = []
+for i in range(1, N):
+    lst.append(sensor[i]-sensor[i-1])
+lst.sort()
+print(sum(lst[:N-K]))
