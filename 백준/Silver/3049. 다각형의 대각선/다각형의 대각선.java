8d695ac823f449844aa1b542c8c57370059d7098
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-08-31 09:32:56 +0900

    [Silver V] Title: 다각형의 대각선, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Silver/3049. 다각형의 대각선/다각형의 대각선.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int answer = ((n-3)*(n-2)*(n-1)*n)/24;
+        System.out.println(answer);
+    }
+}
