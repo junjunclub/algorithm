46cf6d6aac77c1b631a560b434a632414e8492bb
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-09-07 21:09:17 +0900

    [Silver III] Title: 수리공 항승, Time: 68 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        int now;
+        int tape = 0;
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            now = arr[i];
+            if (now > tape) {
+                tape = now + m - 1;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
