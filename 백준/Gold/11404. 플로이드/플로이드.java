acd8f7340d5d32bd84f819f5198f0fa1e5e9ef9d
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-10-28 21:46:35 +0900

    [Gold IV] Title: 플로이드, Time: 312 ms, Memory: 43640 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringTokenizer st;
+    static int MAX_VALUE = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n+1][n+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    arr[i][j] = 0;
+                } else {
+                    arr[i][j] = MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (arr[i][j] > arr[i][k]+arr[k][j]) {
+                        arr[i][j] = arr[i][k]+arr[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (arr[i][j] == MAX_VALUE) {
+                    arr[i][j] = 0;
+                }
+                sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
