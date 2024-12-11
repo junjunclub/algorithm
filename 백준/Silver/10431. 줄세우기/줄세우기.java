96bbedafb3cbd3ad9a96015ea915446de64653fa
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-12-11 14:34:52 +0900

    [Silver V] Title: 줄세우기, Time: 148 ms, Memory: 16320 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            List<Integer> list = new ArrayList<>();
+            int cnt = 0;
+            for (int j = 0; j < 20; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                list.add(num);
+                Collections.sort(list);
+                int temp = list.indexOf(num);
+                cnt += (list.size() - 1) - temp;
+            }
+            sb.append(n).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
