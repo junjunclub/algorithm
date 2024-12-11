de0582e96aef042ab2110b3ba52009f288209dbf
junvelop <151982401+junjunclub@users.noreply.github.com>
2024-12-11 15:02:22 +0900

    [Silver V] Title: 줄세우기, Time: 136 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.java
+++ 백준/Silver/10431. 줄세우기/줄세우기.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -17,15 +16,35 @@ public class Main {
             int n = Integer.parseInt(st.nextToken());
             List<Integer> list = new ArrayList<>();
             int cnt = 0;
+            
             for (int j = 0; j < 20; j++) {
                 int num = Integer.parseInt(st.nextToken());
-                list.add(num);
-                Collections.sort(list);
-                int temp = list.indexOf(num);
-                cnt += (list.size() - 1) - temp;
+
+                // 이진 탐색으로 삽입 위치 찾기
+                int pos = binarySearch(list, num);
+
+                // 삽입 위치 이후의 원소 개수만큼 inversions 발생
+                cnt += list.size() - pos;
+
+                // 해당 위치에 숫자 삽입
+                list.add(pos, num);
             }
             sb.append(n).append(" ").append(cnt).append("\n");
         }
         System.out.println(sb);
+    }
+
+    // 이진 탐색으로 삽입 위치 찾기
+    private static int binarySearch(List<Integer> list, int target) {
+        int left = 0, right = list.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (list.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left; // 삽입 위치
     }
 }
