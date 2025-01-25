f5aca4a97f182f0200e5f74061007a98e76af78f
junvelop <151982401+junjunclub@users.noreply.github.com>
2025-01-25 15:47:10 +0900

    [Bronze V] Title: 제리와 톰, Time: 68 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+        sb.append(m-n).append(" ").append(m);
+        System.out.println(sb);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+    }
+}
