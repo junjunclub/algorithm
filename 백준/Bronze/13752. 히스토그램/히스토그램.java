9f845797721ae7fe965a299aca25489467712eaa
junvelop <151982401+junjunclub@users.noreply.github.com>
2025-02-11 21:42:56 +0900

    [Bronze IV] Title: 히스토그램, Time: 72 ms, Memory: 11576 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            for (int j = 0; j < num; j++) {
+                sb.append("=");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
