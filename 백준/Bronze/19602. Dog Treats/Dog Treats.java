908700bc3c32e143febac8d98ebe4dfb03632345
junvelop <151982401+junjunclub@users.noreply.github.com>
2025-04-11 23:27:06 +0900

    [Bronze IV] Title: Dog Treats, Time: 64 ms, Memory: 11484 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int s = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int l = Integer.parseInt(br.readLine());
+
+        int score = 1*s + 2*m + 3*l;
+
+        if (score >= 10)
+            System.out.println("happy");
+        else
+            System.out.println("sad");
+    }
+}
